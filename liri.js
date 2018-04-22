47ea68d973279dcb8b4883bd69d62717ae94fc80
Majid Jamaleldine <majid.jamaleldine@gmail.com>
2018-04-22 17:56:24 -0500

    got Twitter API call to work

--- liri.js
+++ liri.js
@@ -1,35 +1,28 @@
+
 require("dotenv").config();
+
+// Variables
+const Twitter = require('twitter');
+const Spotify = require('node-spotify-api');
+const fs = require('fs');
 const keys = require('./keys');
-
-let input = process.argv[2];
-let input2 = process.argv[3];
-
 const spotify = new Spotify(keys.spotify);
 const client = new Twitter(keys.twitter);
-
-
-switch (input) {
-    case `my-tweets`:
-        getTweets()
-        break;
-    case `spotify-this-song`:
-        getSongInfo(input2);
-        break;
-    case `movie-this`:
-        getMovieInfo(input2);
-        break;
-    case `do-what-it-says`:
-        followCommands();
-        break;
-    default:
-        console.log('Sorry, ' + expr + ' is not a permitted action.');
-        break;
-}
-
-
+const input = process.argv[2];
+const input2 = process.argv[3];
+const tweetCount = 10;
 
 const getTweets = function() {
-
+    client.get('statuses/user_timeline', {count: `${tweetCount}`}, function(error, tweets, response) {
+        if (!error) {
+            tweets.forEach(info => {
+                console.log(info.text);
+                console.log(info.created_at);
+            })
+        } else {
+            console.log('There was an error: ' + error);
+        }
+    });
 }
 
 const getSongInfo = function(songName) {
@@ -40,6 +33,25 @@ const getMovieInfo = function(movieName) {
 
 }
 
-const followCommands = function() {
-    
+const followCommands = function(commands) {
+
+}
+
+
+switch (input) {
+    case `my-tweets`:
+        getTweets();
+        break;
+    case `spotify-this-song`:
+        getSongInfo(input2);
+        break;
+    case `movie-this`:
+        getMovieInfo(input2);
+        break;
+    case `do-what-it-says`:
+        followCommands(inputCommands);
+        break;
+    default:
+        console.log('Sorry, ' + input + ' is not a permitted action.');
+        break;
 }
